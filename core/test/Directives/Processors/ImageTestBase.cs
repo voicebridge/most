cd1294c3563b271fa60e06ea7ea248c82f92dd54
Graham Plumb <g.plumb@gmail.com>
2019-01-10 17:44:52 +0000

    Addition of ResponsiveImage tests

---
+++ core/test/Directives/Processors/ImageTestBase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VoiceBridge.Most.Directives;
+using VoiceBridge.Most.Directives.Processors;
+using VoiceBridge.Most.VoiceModel.Alexa;
+using VoiceBridge.Most.VoiceModel.Alexa.APL;
+using VoiceBridge.Most.VoiceModel.Alexa.Directives;
+using Xunit;
+
+namespace VoiceBridge.Most.Test.Directives.Processors
+{
+    public abstract class ImageTestBase
+    {
+        protected abstract IImage GetImage();
+
+        protected IImage ExecuteProcessor(Action<IVirtualDirective, ImageProcessor> action, IImage image = null)
+        {
+            var item = (image == null) ? GetImage() : image;
+            var virtualDirective = new ImageDirective()
+            {
+                Image = item
+            };
+
+            var processor = new ImageProcessor();
+            action(virtualDirective, processor);
+            return item;
+        }
+
+        protected void TestAplDocument(ShowImageDirective directive)
+        {
+            Assert.NotNull(directive);
+            Assert.Null(directive.DataSources);
+            Assert.Equal(AlexaConstants.Presentation.Directives.RenderDocument, directive.Type);
+            Assert.Equal("1.0", directive.Document.Version);
+            Assert.Equal("APL", directive.Document.Type);
+            Assert.Equal("auto", directive.Document.Theme);
+            Assert.Empty(directive.Document.Resources);
+            Assert.Single(directive.Document.Import);
+            Assert.Equal("1.0.0", directive.Document.Import[0].Version);
+            Assert.Equal("alexa-viewport-profiles", directive.Document.Import[0].Name);
+            Assert.Empty(directive.Document.MainTemplate.Parameters);
+            Assert.Single(directive.Document.MainTemplate.Items);
+        }
+
+        protected void TestParentContainer(TemplateContainer container, int expectedChildren)
+        {
+            Assert.Equal(expectedChildren, container.Items.Count);
+            Assert.True(string.IsNullOrWhiteSpace(container.AlignItems));
+            Assert.Equal("column", container.Direction);
+            Assert.Equal(0, container.Grow);
+            Assert.True(string.IsNullOrWhiteSpace(container.JustifyContent));
+            Assert.Equal("Container", container.Type);
+            Assert.Null(container.When);
+        }
+
+        protected void TestContainerWithImage(TemplateContainer container, string url, string when = null)
+        {
+            Assert.Single(container.Items);
+            Assert.Equal("center", container.AlignItems);
+            Assert.True(string.IsNullOrWhiteSpace(container.Direction));
+            Assert.Equal(1, container.Grow);
+            Assert.Equal("center", container.JustifyContent);
+            Assert.Equal("Container", container.Type);
+
+            if (when == null)
+            {
+                Assert.Null(container.When);
+            }
+            else
+            {
+                Assert.Equal(when, container.When);
+            }
+
+            var tImage = container.Items[0] as TemplateImage;
+            Assert.Equal("center", tImage.Align);
+            Assert.Equal("100vh", tImage.Height);
+            Assert.Equal("best-fill", tImage.Scale);
+            Assert.Equal(url, tImage.Source);
+            Assert.Equal("Image", tImage.Type);
+            Assert.Equal("100vw", tImage.Width);
+        }
+    }
+}
---
+++ core/test/Directives/Processors/ResponsiveImageTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VoiceBridge.Most.Directives;
+using VoiceBridge.Most.Directives.Processors;
+using VoiceBridge.Most.Google;
+using VoiceBridge.Most.Test.TestData;
+using VoiceBridge.Most.VoiceModel.Alexa;
+using VoiceBridge.Most.VoiceModel.Alexa.APL;
+using VoiceBridge.Most.VoiceModel.Alexa.Directives;
+using Xunit;
+
+namespace VoiceBridge.Most.Test.Directives.Processors
+{
+    public class ResponsiveImageTest : ImageTestBase
+    {
+        private const string IMAGE_URL_1 = "https://images-na.ssl-images-amazon.com/images/I/61GcN5yX6XL._SL1000_.jpg";
+        private const string IMAGE_URL_2 = "https://images-na.ssl-images-amazon.com/images/I/6182S7MYC2L._SL1000_.jpg";
+        private const string IMAGE_URL_3 = "https://images-na.ssl-images-amazon.com/images/I/61yI7vWa83L._SL1000_.jpg";
+        private const string IMAGE_URL_4 = "https://images-na.ssl-images-amazon.com/images/I/51FQBzDZ3VL.jpg";
+
+
+        [Fact]
+        public void ShowImageOnAlexa()
+        {
+            var request = AlexaRequests.Boilerplate();
+            request.Context.System.Device.SupportedInterfaces.Add(AlexaConstants.DeviceInterfaceNames.AlexaPresentationLanguage, "");
+
+            var response = AlexaResponses.Boilerplate();
+            var image = ExecuteProcessor((virtualDirective, processor) => processor.Process(virtualDirective, request, response));
+            var directive = (ShowImageDirective)response.Content.Directives.Single(d => d is ShowImageDirective);
+
+            Assert.True(image is ResponsiveImage);
+
+            TestAplDocument(directive);
+
+            var parent = directive.Document.MainTemplate.Items[0];
+            TestParentContainer(parent, 4);
+
+            var item = parent.Items[0] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_1, AlexaConstants.Presentation.TemplateItems.ViewportClauses.SmallRound);
+
+            item = parent.Items[1] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_2, AlexaConstants.Presentation.TemplateItems.ViewportClauses.MediumRectangle);
+
+            item = parent.Items[2] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_3, AlexaConstants.Presentation.TemplateItems.ViewportClauses.LargeRectangle);
+
+            item = parent.Items[3] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_4, AlexaConstants.Presentation.TemplateItems.ViewportClauses.ExtraLargeRectangle);
+        }
+
+
+        [Fact]
+        public void ShowImageOnAlexaWithFallback()
+        {
+            var request = AlexaRequests.Boilerplate();
+            request.Context.System.Device.SupportedInterfaces.Add(AlexaConstants.DeviceInterfaceNames.AlexaPresentationLanguage, "");
+
+            // LargeImageUri will "fall back" to ExtraLargeImageUri by the removal of the latter's "when" predicate
+            var image = new ResponsiveImage()
+            {
+                SmallImageUri = new Uri(IMAGE_URL_1),
+                MediumImageUri = new Uri(IMAGE_URL_2),
+                ExtraLargeImageUri = new Uri(IMAGE_URL_4),
+            };
+
+            var response = AlexaResponses.Boilerplate();
+            var result = ExecuteProcessor((virtualDirective, processor) => processor.Process(virtualDirective, request, response), image);
+            var directive = (ShowImageDirective)response.Content.Directives.Single(d => d is ShowImageDirective);
+
+            Assert.Equal(image, result);
+
+            TestAplDocument(directive);
+
+            var parent = directive.Document.MainTemplate.Items[0];
+            TestParentContainer(parent, 3);
+
+            var item = parent.Items[0] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_1, AlexaConstants.Presentation.TemplateItems.ViewportClauses.SmallRound);
+
+            item = parent.Items[1] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_2, AlexaConstants.Presentation.TemplateItems.ViewportClauses.MediumRectangle);
+
+            item = parent.Items[2] as TemplateContainer;
+            TestContainerWithImage(item, IMAGE_URL_4);
+        }
+
+
+        [Fact]
+        public void ShowImageOnAlexaNullUrls()
+        {
+            var request = AlexaRequests.Boilerplate();
+            request.Context.System.Device.SupportedInterfaces.Add(AlexaConstants.DeviceInterfaceNames.AlexaPresentationLanguage, "");
+
+            var response = AlexaResponses.Boilerplate();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var image = ExecuteProcessor((virtualDirective, processor) => processor.Process(virtualDirective, request, response), new ResponsiveImage());
+            });
+        }
+
+
+        [Fact]
+        public void ShowImageOnAlexaNoSupport()
+        {
+            var request = AlexaRequests.Boilerplate();
+            var response = AlexaResponses.Boilerplate();
+            var image = ExecuteProcessor((virtualDirective, processor) => processor.Process(virtualDirective, request, response));
+
+            Assert.Empty(response.Content.Directives);
+        }
+
+
+        [Fact]
+        public void ShowImageOnGoogle()
+        {
+            var request = AppRequests.CreateBoileRequest();
+            var response = new ActionResponseFactory().Create(new ConversationContext());
+            var image = ExecuteProcessor((virtualDirective, processor) => processor.Process(virtualDirective, request, response));
+
+            Assert.Empty(response.Messages);
+            Assert.Empty(response.Payload.Body.RichResponse.Items);
+        }
+
+
+        protected override IImage GetImage()
+        {
+            return new ResponsiveImage()
+            {
+                SmallImageUri = new Uri(IMAGE_URL_1),
+                MediumImageUri = new Uri(IMAGE_URL_2),
+                LargeImageUri = new Uri(IMAGE_URL_3),
+                ExtraLargeImageUri = new Uri(IMAGE_URL_4),
+            };
+        }
+    }
+}
