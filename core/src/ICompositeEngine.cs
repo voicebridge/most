59dd65f83776b57512b2595f8b38f0b70d10b58a
Abdellah Elamiri <aezbox@gmail.com>
2019-01-22 21:17:52 -0800

    Added Composite Engine

--- core/src/ICompositeEngine.cs
+++ core/src/ICompositeEngine.cs
@@ -1,7 +1,10 @@
+using System.Threading.Tasks;
+using VoiceBridge.Most.VoiceModel;
+
 namespace VoiceBridge.Most
 {
     public interface ICompositeEngine
     {
-        
+        Task<IResponse> Evaluate(IRequest request);
     }
 }
