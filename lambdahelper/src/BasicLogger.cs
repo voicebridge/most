5021adb4fa55c73f5e75ed278099d47e3641ce90
Abdellah Elamiri <aezbox@gmail.com>
2019-01-24 02:46:58 -0800

    Added BasicLambdaLogger

---
+++ lambdahelper/src/BasicLogger.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Amazon.Lambda.Core;
+using VoiceBridge.Most.Logging;
+
+namespace VoiceBridge.Most.LambdaHelper
+{
+    public class BasicLogger : ILogger
+    {
+        public void Log(LogLevel level, string message, params object[] formattingArgs)
+        {
+            try
+            {
+                var msg = formattingArgs != null && formattingArgs.Any()
+                    ? string.Format(message, formattingArgs)
+                    : message;
+                msg = $"[{level}] {msg}";
+                LambdaLogger.Log(msg);
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log($"Badly formatted message: {message} ERROR: {e}");
+            }
+        }
+    }
+}
